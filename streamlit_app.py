e55cd4a02eb778780173082ad76ccf11a3938cd4
SaiGnapikaVajrala <saignapika@synthlake.com>
2026-02-04 14:30:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ pd_df = my_dataframe.to_pandas()
 Ingredients_list = st.multiselect(
     'choose up to 5 ingredients:',
     my_dataframe,
-    max_selections = 5
+    
     
 )
 if Ingredients_list:
